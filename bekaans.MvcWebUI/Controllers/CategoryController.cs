c3f2d2e2d954fe158c0cc77bb6019a6d86f3a98d
Berke Kaan <bekaans@icloud.com>
2023-09-05 19:46:53 +0300

    .gitignore

---
+++ bekaans.MvcWebUI/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using bekaans.Entities;
+using bekaans.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bekaans.MvcWebUI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        ICategoryService _categoryservice;
+
+        public CategoryController(ICategoryService categoryservice)
+        {
+            _categoryservice = categoryservice;
+        }
+
+        public PartialViewResult List(int category =0)
+        {
+            ViewBag.CurrentCategory = category;
+            return PartialView(_categoryservice.GetAll());
+        }
+    }
+}
