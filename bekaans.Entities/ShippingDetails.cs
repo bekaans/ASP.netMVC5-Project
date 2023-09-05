7d75a7a4fdc48d2c83e5b53c55c17232a93d7a0a
Berke Kaan <bekaans@icloud.com>
2023-09-05 21:28:08 +0300

    Cart Checkoout

---
+++ bekaans.Entities/ShippingDetails.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bekaans.Entities
+{
+    public class ShippingDetails
+    {
+        [Required(ErrorMessage ="You need to write your name")]
+        public string Name { get; set; }
+        [Required]
+        [MinLength(10)]
+        [MaxLength(50)]
+        public string Addres1 { get; set; }
+        [MinLength(10)]
+        [MaxLength(50)]
+        public string Addres2 { get; set; }
+        [MinLength(10)]
+        [MaxLength(50)]
+        public string Addres3 { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Country { get; set; }
+        public string isGift { get; set; }
+    }
+}
--- bekaans.MvcWebUI/Controllers/CartController.cs
+++ bekaans.MvcWebUI/Controllers/CartController.cs
@@ -11,13 +11,17 @@ namespace bekaans.MvcWebUI.Controllers
     public class CartController : Controller
     {
         IProductService _productservice;
-
+        public ActionResult Index()
+        {
+            var cart = (Cart)Session["cart"];
+            return View(cart);
+        }
         public CartController(IProductService productservice)
         {
             _productservice = productservice;
         }
 
-        public ActionResult AddToCart(int productid)
+        public RedirectToRouteResult AddToCart(int productid)
         {
             Product product  = _productservice.Get(productid);
             var cart = (Cart)Session["cart"];
@@ -27,7 +31,32 @@ namespace bekaans.MvcWebUI.Controllers
                 Session["cart"] = cart;
             }
             cart.AddToCart(product, 1);
-            return View("Index");
+            return RedirectToAction("Index",cart);
+        }
+        public RedirectToRouteResult RemoveFromCart(int productid) 
+        {
+            Product product = _productservice.Get(productid);
+            var cart = (Cart)Session["cart"];
+            cart.RemoveFromCart(product);
+            return RedirectToAction("Index",cart);
+        }
+        [HttpGet]
+        public ActionResult Checkout()
+        {
+            return View(new ShippingDetails());
+        }
+        [HttpPost]
+        public ActionResult Checkout(ShippingDetails shippingDetails)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Complated");
+            }
+            else
+            {
+                return View(shippingDetails);
+            }
+           
         }
     }
 }
