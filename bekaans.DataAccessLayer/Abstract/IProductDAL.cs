2fe421b2bfbff7fdf32ca7f74c91cdbe4159cfcc
Berke Kaan <bekaans@icloud.com>
2023-09-05 11:58:27 +0300

    Add bekaans.DAL/IProductDAL

---
+++ bekaans.DataAccessLayer/Abstract/IProductDAL.cs
@@ -0,0 +1,18 @@
+﻿using bekaans.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bekaans.DataAccessLayer.Abstract
+{
+    public interface IProductDA 
+    {
+        List<Product> GetAll();
+        Product Get(int productid);
+        void AddProduct(Product product);
+        void DeleteProduct(int productid);
+        void UpdateProduct(Product product);
+    }
+}
