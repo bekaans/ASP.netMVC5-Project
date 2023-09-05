da3d13ff88c5c383beb67a5987e6fc7f9f75021d
Berke Kaan <bekaans@icloud.com>
2023-09-05 16:01:07 +0300

    Add Interfaces/ProductManager

---
+++ bekaans.BLL/Concrete/ProductManager.cs
@@ -0,0 +1,40 @@
+﻿using bekaans.DataAccessLayer.Abstract;
+using bekaans.Entities;
+using bekaans.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bekaans.BLL.Concrete
+{
+    IProductDAL
+    public class ProductManager : IProductService
+    {
+        public void AddProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteProduct(int product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
