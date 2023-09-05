71058a5dca267b7a24b489b7dc83ffe845b626a9
Berke Kaan <bekaans@icloud.com>
2023-09-05 16:24:39 +0300

    .gitignore

--- bekaans.Interfaces/IProductService.cs
+++ bekaans.Interfaces/IProductService.cs
@@ -2,17 +2,33 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace bekaans.Interfaces
-{
+{   
+    [ServiceContract]
     public interface IProductService
     {
+
+        [OperationContract]
         List<Product> GetAll();
+
+
+        [OperationContract]
         Product Get(int productid);
+
+
+        [OperationContract]
         void AddProduct(Product product);
+
+
+        [OperationContract]
         void DeleteProduct(int product);
+
+
+        [OperationContract]
         void UpdateProduct(Product product);
     }
 }
